2f7f295d8295b08fb84870dfe0b0b94624873d53
Rouak-stha <rounakshrestha1153@gmail.com>
2023-03-11 11:19:37 +0545

    First Commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            blockquote: {
+              'background': theme('colors.gray.600'),
+              'border-left-color': theme('colors.zinc.400'),
+              'content': 'none'
+            },
+            maxWidth: '100%'
+          }
+        }
+      })
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
+}
